ace48b12f8d1190869c7527f533d9f4c5d68459b
lelethungalo@gmail.com <leletungalo@gmail.com>
2019-06-23 22:28:40 +0200

    Updates

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let dataArr = new Array();
+//Funstion called by on submit
 function submitForm(){
+    //getting form from the document object
     let form = document.forms["myForm"]; 
+    //getting button from form
     let password = form["password"].value;
+    //getting button from form
     let confimPassword = form["Cpassword"].value;
-    
+     
+    //Compere passwords
     if(password == confimPassword && password != ""){
         dataArr.push(form["Name"].value);
         dataArr.push(form["Surname"].value);
